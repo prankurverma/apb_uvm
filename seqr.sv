9cc7b024eb89f7285f25984bf5bf6b62d2736f72
Prankur Verma <vprankur@gmail.com>
2023-07-23 20:43:02 +0530

    Update seqr.sv

--- seqr.sv
+++ seqr.sv
@@ -1,20 +1,23 @@
 class apb_sequencer extends uvm_sequencer #(apb_sequence_item);
   `uvm_component_utils(apb_sequencer)
+  
   function new(string name = "apb_sequencer", uvm_component parent=null);
-  super.new(name, parent);
+    super.new(name, parent);
   endfunction
+  
   function void build_phase(uvm_phase phase);
-  super.build_phase(phase);
-  `uvm_info(get_type_name(), " Build Phase ", UVM_HIGH);
+    super.build_phase(phase);
+    `uvm_info(get_type_name(), " Build Phase ", UVM_HIGH);
   endfunction
   
   function void connect_phase(uvm_phase phase);
-  super.connect_phase(phase);
-  `uvm_info(get_type_name(), " Connect Phase ", UVM_HIGH);
+    super.connect_phase(phase);
+    `uvm_info(get_type_name(), " Connect Phase ", UVM_HIGH);
   endfunction
-  task run_phase(uvm_phase phase);
-  super.run_phase(phase);
-  `uvm_info(get_type_name(), "In run phase", UVM_HIGH);
-  endtask
-  endclass
   
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    `uvm_info(get_type_name(), "In run phase", UVM_HIGH);
+  endtask
+  
+endclass
